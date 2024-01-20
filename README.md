be61a30cfef927cc4b82e9621fb7b4a3ff462f6e
Benign X <1341398182@qq.com>
2024-01-20 23:23:23 +0800

    😁(Update): Update
    Signed-off-by: Benign X <1341398182@qq.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Font2Img
 A Freetype Playground.
+
+# How to build
+```shell
+meson compile -C build
+```
--- font2img.cpp
+++ font2img.cpp
@@ -1,12 +1,20 @@
+#include <ft2build.h>
 #include <iostream>
+#include FT_FREETYPE_H
 
 #define PROJECT_NAME "Font2Img"
 
 int main(int argc, char **argv) {
-    if(argc != 1) {
-        std::cout << argv[0] <<  "takes no arguments.\n";
+    FT_Library library;
+    FT_Init_FreeType(&library);
+
+    if (argc != 1) {
+        std::cout << argv[0] << "takes no arguments.\n";
         return 1;
     }
     std::cout << "This is project " << PROJECT_NAME << ".\n";
+    std::cout << library << std::endl;
+
+    FT_Done_FreeType(library);
     return 0;
 }
